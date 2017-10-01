9b041f79e70bf8c2a699e31d5419724449366983
parinz1234 <parinya.ons@gmail.com>
2017-10-01 10:46:34 +0700

    init clock

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot){
   module.hot.accept()
 }
 
-registerServiceWorker();
+const tick = () => ReactDOM.render(<App />, document.getElementById('root'))
+
+setInterval(tick, 1000)
