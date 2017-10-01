5d744a0351c154059f4e6121fcf759e42cf09c9f
parinz1234 <parinya.ons@gmail.com>
2017-10-02 00:33:14 +0700

    add stateless

--- src/App.js
+++ src/App.js
@@ -74,52 +74,39 @@ class App extends Component {
 }
 
 // search
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children}
+    <input 
+      type="text"
+      onChange={onChange}
+      value={value}
+      />
+  </form>
 
-class Search extends Component {
-  render () {
-    const { value, onChange, children } = this.props
-    return (
-      <form>
-        {children}
-        <input 
-          type="text"
-          onChange={onChange}
-          value={value}
-          />
-      </form>
-    )
-  }
-}
 
 
 // table
-class Table extends Component {
-  /* don't use index of array as key because isn't stable */
-  render () {
-    const { list, searchTerm, onDismiss } = this.props
-    return (
-      <div>
-        {
-          list.filter(isSearched(searchTerm)).map((item) => {
-            return (
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                  <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
-                </span>
-              </div>
-            )
-          })
-        }
-      </div>
-    )
-  }
-}
+const Table = ({ list, searchTerm, onDismiss }) =>
+  <div>
+    {
+      list.filter(isSearched(searchTerm)).map((item) => {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+            </span>
+          </div>
+        )
+      })
+    }
+  </div>
 
 // BUtton
 class Button extends Component {
